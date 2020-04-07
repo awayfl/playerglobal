ca3024ef18383697de073ce890c56b534a7d3d5f
80prozent <80prozent@differentdesign.de>
2020-04-07 18:30:51 +0200

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - PlayerGlobal - 0.2.8");
+console.debug("AwayFL - PlayerGlobal - 0.2.9");
 
 export { Sprite } from "./lib/display/Sprite";
 export { FlashSceneGraphFactory } from "./lib/factories/FlashSceneGraphFactory";
