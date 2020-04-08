fb632d0be9e9a48591dbe8b41c551aa7015bdc08
eXponenta <rondo.devil@gmail.com>
2020-04-08 19:35:21 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - PlayerGlobal - 0.2.10");
+console.debug("AwayFL - PlayerGlobal - 0.2.11");
 
 export { Sprite } from "./lib/display/Sprite";
 export { FlashSceneGraphFactory } from "./lib/factories/FlashSceneGraphFactory";
