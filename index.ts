c3cc1c6d13792a4eb35cf2fe40e7ed51f0d5d881
Rob Bateman <rob.bateman@gmail.com>
2020-06-09 13:40:11 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - PlayerGlobal - 0.2.61");
+console.debug("AwayFL - PlayerGlobal - 0.2.62");
 
 export { Sprite } from "./lib/display/Sprite";
 export { FlashSceneGraphFactory } from "./lib/factories/FlashSceneGraphFactory";
