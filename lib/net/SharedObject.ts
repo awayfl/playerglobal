899a349284e9dd4b97186e11fb4caf89c9f0a81e
Exponenta <rondo.devil@gmail.com>
2020-07-10 11:42:51 +0300

    Fix UNTRACEABLE issues
    Local Storage issue on Incognito

--- lib/net/SharedObject.ts
+++ lib/net/SharedObject.ts
@@ -1,31 +1,44 @@
-import { ASObject } from '@awayfl/avm2';
-import { notImplemented, warning, StringUtilities } from '@awayfl/swf-loader';
-import { axCoerceString, ObjectEncoding, AMF3 } from '@awayfl/avm2';
-import { ByteArray } from '../utils/ByteArray';
-import { SecurityDomain } from '../SecurityDomain';
+import { ASObject } from "@awayfl/avm2";
+import { notImplemented, warning, StringUtilities } from "@awayfl/swf-loader";
+import { axCoerceString, ObjectEncoding, AMF3 } from "@awayfl/avm2";
+import { ByteArray } from "../utils/ByteArray";
+import { SecurityDomain } from "../SecurityDomain";
+
 
 interface IStorage {
 	getItem(key: string): string;
 	setItem(key: string, value: string): void;
 	removeItem(key: string): void;
-  }
-  
-  var _sharedObjectStorage: IStorage;
-function getSharedObjectStorage(): IStorage  {
-	/*if (!_sharedObjectStorage) {
-	  if (typeof ShumwayCom !== 'undefined' && ShumwayCom.createSpecialStorage) {
-		_sharedObjectStorage = ShumwayCom.createSpecialStorage();
-	  } else {
-		_sharedObjectStorage = (<any>window).sessionStorage;
-	  }
+}
+
+let _sharedObjectStorage: IStorage;
+
+class VirtualStorage implements IStorage {
+	_values: StringMap<string> = {};
+	getItem(key: string) {
+		return this._values[key];
 	}
-	release || assert(_sharedObjectStorage, "SharedObjectStorage is not available.");
-	*/
-	if (typeof (Storage) !== "undefined") {
-		_sharedObjectStorage=localStorage;
+
+	setItem(key: string, value: string) {
+		this._values[key] = value;
+	}
+	removeItem(key: string) {
+		delete this._values[key];
+	}
+}
+
+function getSharedObjectStorage(): IStorage {
+	if (typeof Storage !== "undefined" && !_sharedObjectStorage) {
+		try{
+			_sharedObjectStorage = window.localStorage;
+		} catch(e) {
+			console.warn("[Shared Storage] ", e);
+			_sharedObjectStorage = new VirtualStorage();
+		}
 	}
 	return _sharedObjectStorage;
-  }
+}
+
 export class SharedObject extends ASObject {
 	private _data: ASObject;
 	private _object_name: string;
@@ -39,31 +52,33 @@ export class SharedObject extends ASObject {
 		super();
 	}
 
-
 	static get defaultObjectEncoding(): number /*uint*/ {
-		notImplemented("public flash.net.SharedObject::static defaultObjectEncoding"); return;
+		notImplemented("public flash.net.SharedObject::static defaultObjectEncoding");
+		return;
 	}
 	static set defaultObjectEncoding(version: number /*uint*/) {
-		notImplemented("public flash.net.SharedObject::static defaultObjectEncoding"); return;
+		notImplemented("public flash.net.SharedObject::static defaultObjectEncoding");
+		return;
 	}
 	public static deleteAll(url: string): number /*int*/ {
-		notImplemented("public flash.net.SharedObject::static deleteAll"); return;
+		notImplemented("public flash.net.SharedObject::static deleteAll");
+		return;
 	}
 	public static getDiskUsage(url: string): number /*int*/ {
 		notImplemented("public flash.net.SharedObject::static getDiskUsage");
 		return 0;
 	}
-	
+
 	public _path: string;
 	public _fps: number;
 	public _objectEncoding;
 	private static _defaultObjectEncoding = ObjectEncoding.DEFAULT;
-	
+
 	public static getLocal(name: string, localPath?: string, secure?: boolean): SharedObject {
 		name = axCoerceString(name);
 		localPath = axCoerceString(localPath);
 		secure = !!secure;
-		var path = (localPath || '') + '/' + name;
+		var path = (localPath || "") + "/" + name;
 		/*if (this._sharedObjects[path]) {
 		return this._sharedObjects[path];
 		}*/
@@ -71,37 +86,36 @@ export class SharedObject extends ASObject {
 		var data;
 		var encoding = this._defaultObjectEncoding;
 		if (encodedData) {
-		try {
-			var bytes = StringUtilities.decodeRestrictedBase64ToBytes(encodedData);
-			//console.log("loaded bytes", bytes);
-			var serializedData = new ByteArray(bytes.length);
-			(<any>serializedData).sec=this.sec;
-			serializedData.setArrayBuffer(bytes.buffer);
-			
-			//console.log("serializedData.arraybytes", serializedData.arraybytes);
-			
-				
-			//	unexpected("Object Encoding");
-			//}
-			data = AMF3.read(<any>serializedData); //serializedData.readObject();
-			//encoding = serializedData.objectEncoding;
-		} catch (e) {
-			warning('Error encountered while decoding LocalStorage entry. Resetting data.');
-		}
-		if (!data || typeof data !== 'object') {
+			try {
+				var bytes = StringUtilities.decodeRestrictedBase64ToBytes(encodedData);
+				//console.log("loaded bytes", bytes);
+				var serializedData = new ByteArray(bytes.length);
+				(<any>serializedData).sec = this.sec;
+				serializedData.setArrayBuffer(bytes.buffer);
+
+				//console.log("serializedData.arraybytes", serializedData.arraybytes);
+
+				//	unexpected("Object Encoding");
+				//}
+				data = AMF3.read(<any>serializedData); //serializedData.readObject();
+				//encoding = serializedData.objectEncoding;
+			} catch (e) {
+				warning("Error encountered while decoding LocalStorage entry. Resetting data.");
+			}
+			if (!data || typeof data !== "object") {
+				data = this.sec.createObject();
+			}
+		} else {
 			data = this.sec.createObject();
 		}
-		} else {
-		data = this.sec.createObject();
-		}
-		var so:SharedObject = new (<SecurityDomain> this.sec).flash.net.SharedObject();
-		so._path=path;
+		var so: SharedObject = new (<SecurityDomain>this.sec).flash.net.SharedObject();
+		so._path = path;
 		so._objectEncoding = encoding;
-		so._data=data;
+		so._data = data;
 		return so;
 	}
 	public static getRemote(name: string, remotePath?: string, persistence?: boolean, secure?: boolean): SharedObject {
-		return new (<SecurityDomain> this.sec).flash.net.SharedObject();
+		return new (<SecurityDomain>this.sec).flash.net.SharedObject();
 	}
 
 	public flush(minDiskSpace: number = 0): void {
@@ -114,24 +128,23 @@ export class SharedObject extends ASObject {
 		}*/
 		minDiskSpace = minDiskSpace | 0;
 
-		
 		// Check if the object is empty. If it is, don't create a stored object if one doesn't exist.
 		var isEmpty = true;
 		for (var key in this._data) {
-		  if (this._data.hasOwnProperty(key)) {
-			isEmpty = false;
-			break;
-		  }
+			if (this._data.hasOwnProperty(key)) {
+				isEmpty = false;
+				break;
+			}
 		}
 		if (isEmpty && !getSharedObjectStorage().getItem(this._path)) {
-		  return;
+			return;
 		}
 		var serializedData = new ByteArray();
-		(<any>serializedData).sec=this.sec;
+		(<any>serializedData).sec = this.sec;
 		//serializedData.objectEncoding = this._objectEncoding;
 		//serializedData.writeObject(this._data);
-		
-        AMF3.write(<any>serializedData, this._data);
+
+		AMF3.write(<any>serializedData, this._data);
 		//data = AMF3.write(<any>serializedData); //serializedData.readObject();
 		var bytes = new Uint8Array(serializedData.arraybytes);
 		var encodedData = StringUtilities.base64EncodeBytes(bytes);
@@ -145,60 +158,69 @@ export class SharedObject extends ASObject {
 		getSharedObjectStorage().setItem(this._path, encodedData);
 	}
 
-
 	public get data(): ASObject {
 		return this._data;
 	}
 	public set data(object: ASObject) {
-		this._data=object;
+		this._data = object;
 	}
 
 	public get objectEncoding(): number /*uint*/ {
-		notImplemented("public flash.net.SharedObject::get objectEncoding"); return;
+		notImplemented("public flash.net.SharedObject::get objectEncoding");
+		return;
 	}
 	public set objectEncoding(version: number /*uint*/) {
-		notImplemented("public flash.net.SharedObject::set objectEncoding"); return;
+		notImplemented("public flash.net.SharedObject::set objectEncoding");
+		return;
 	}
 
 	public get client(): ASObject {
-		notImplemented("public flash.net.SharedObject::get client"); return;
+		notImplemented("public flash.net.SharedObject::get client");
+		return;
 		// return this._client;
 	}
 	public set client(object: ASObject) {
-		notImplemented("public flash.net.SharedObject::set client"); return;
+		notImplemented("public flash.net.SharedObject::set client");
+		return;
 		// this._client = object;
 	}
 	public setDirty(propertyName: string): void {
-		notImplemented("public flash.net.SharedObject::setDirty"); return;
+		notImplemented("public flash.net.SharedObject::setDirty");
+		return;
 	}
 
 	public connect(myConnection: any, params: string = null): void {
-		notImplemented("public flash.net.SharedObject::connect"); return;
+		notImplemented("public flash.net.SharedObject::connect");
+		return;
 	}
 
 	public send(): void {
-		notImplemented("public flash.net.SharedObject::send"); return;
+		notImplemented("public flash.net.SharedObject::send");
+		return;
 	}
 
 	public close(): void {
-		notImplemented("public flash.net.SharedObject::close"); return;
+		notImplemented("public flash.net.SharedObject::close");
+		return;
 	}
 
-
 	public clear(): void {
-		notImplemented("public flash.net.SharedObject::clear"); return;
+		notImplemented("public flash.net.SharedObject::clear");
+		return;
 	}
 
 	public get size(): number {
-		notImplemented("public flash.net.SharedObject::get size"); return;
+		notImplemented("public flash.net.SharedObject::get size");
+		return;
 	}
 
 	public set fps(updatesPerSecond: number) {
-		notImplemented("public flash.net.SharedObject::set fps"); return;
+		notImplemented("public flash.net.SharedObject::set fps");
+		return;
 	}
 
 	public setProperty(propertyName: string, value: any = null): void {
-		notImplemented("public flash.net.SharedObject::setProperty"); return;
+		notImplemented("public flash.net.SharedObject::setProperty");
+		return;
 	}
-
 }
