98311bec79bcae0112ab556ee435e87d0f63f2b9
80prozent <80prozent@differentdesign.de>
2021-01-15 23:45:53 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - PlayerGlobal - 0.2.168");
+console.debug("AwayFL - PlayerGlobal - 0.2.169");
 
 export { Sprite } from './lib/display/Sprite';
 export { MovieClip } from './lib/display/MovieClip';
