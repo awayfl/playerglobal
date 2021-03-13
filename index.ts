7ee9a0e972dae0c348d4c973cf6a1982604c9f88
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-03-13 22:28:26 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - PlayerGlobal - 0.2.189");
+console.debug("AwayFL - PlayerGlobal - 0.2.190");
 
 export { Sprite } from './lib/display/Sprite';
 export { MovieClip } from './lib/display/MovieClip';
