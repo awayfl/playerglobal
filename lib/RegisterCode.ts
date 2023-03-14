a0c6edbb137b25b446aaf82e20bc0d4ee8e96c4f
Rob Bateman <rob@infiniteturtles.co.uk>
2023-03-14 01:12:20 +0000

    remove unecessary commented code

--- lib/RegisterCode.ts
+++ lib/RegisterCode.ts
@@ -8,17 +8,3 @@ Loader.enableParser(WaveAudioParser);
 Loader.enableParser(Image2DParser);
 Loader.enableParser(ImageCubeParser);
 Loader.enableParser(TextureAtlasParser);
-
-//register view nodes
-/*
-PartitionBase.registerAbstraction(CameraNode, Camera);
-PartitionBase.registerAbstraction(EntityNode, Sprite);
-PartitionBase.registerAbstraction(EntityNode, DisplayObjectContainer);
-PartitionBase.registerAbstraction(EntityNode, Scene);
-PartitionBase.registerAbstraction(EntityNode, MorphSprite);
-PartitionBase.registerAbstraction(EntityNode, MovieClip);
-PartitionBase.registerAbstraction(EntityNode, Billboard);
-PartitionBase.registerAbstraction(EntityNode, LineSegment);
-PartitionBase.registerAbstraction(EntityNode, TextField);
-PartitionBase.registerAbstraction(SkyboxNode, Skybox);
-*/
