2d9bb89e2918f51e00c3b2f4f257faa74a44d38f
Rob Bateman <rob@infiniteturtles.co.uk>
2023-03-17 01:38:02 +0000

    add Settings file to allow enable / disable of experimental showSaveFilePicker() API

---
+++ lib/Settings.ts
@@ -0,0 +1,15 @@
+import { ConfigManager }  from '@awayjs/core';
+
+export interface IPlayerGlobalSettings {
+
+	ENABLE_FILE_PICKER: boolean;
+}
+
+export const Settings: IPlayerGlobalSettings = ConfigManager.instance.addStore<any>('playerglobal',{
+
+	/**
+	 * @description Enable experimental File Picker 
+	 */
+	ENABLE_FILE_PICKER: true,
+
+});
