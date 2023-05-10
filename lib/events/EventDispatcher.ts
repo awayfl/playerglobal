9cc342d5cdd1a5006787db02c4616c4288f16c3a
Rob Bateman <rob@infiniteturtles.co.uk>
2023-05-10 03:20:03 +0100

    add WeakRef to BroadcastEventDispatchQueue

--- lib/events/EventDispatcher.ts
+++ lib/events/EventDispatcher.ts
@@ -2,12 +2,13 @@ import { Event } from './Event';
 import { IEventMapper } from './IEventMapper';
 import { EventDispatcherBase } from './EventDispatcherBase';
 
-import { EventBase } from '@awayjs/core';
+import { EventBase, UUID } from '@awayjs/core';
 import { release } from '@awayfl/swf-loader';
 import { assert } from '@awayjs/graphics';
 import { SecurityDomain } from '../SecurityDomain';
 
 import { MouseEvent } from '../events/MouseEvent';
+import { Settings } from '@awayfl/avm2';
 /**
 	 * [broadcast event] Dispatched when the Flash Player or AIR application operating
 	 * loses system focus and is becoming inactive.
@@ -42,6 +43,8 @@ export class EventDispatcher extends EventDispatcherBase {
 		//KeyboardEvent.KEY_DOWN,
 		//KeyboardEvent.KEY_UP,
 	]
+
+	public readonly id: number;
 
 	public toString(): string {
 		return '';
@@ -85,6 +88,8 @@ export class EventDispatcher extends EventDispatcherBase {
 
 	constructor(target: any = null) {
 		super(target);
+
+		this.id = UUID.Next();
 
 		this.eventMapping || (this.eventMapping = {});
 		this.eventMappingDummys || (this.eventMappingDummys = {});
@@ -211,79 +216,68 @@ export class EventDispatcher extends EventDispatcherBase {
 	}
 }
 
+declare global {
+	interface Window { WeakRef: typeof WeakRef}
+}
+
+const USE_WEAK = ('WeakRef' in self) && Settings.USE_WEAK_REF
+
+if (USE_WEAK) {
+	console.debug('[BroadcastEventDispatchQueue] `WeakRef` used for Broadcast Events!');
+}
+
 export class BroadcastEventDispatchQueue {
 	/**
 	 * The queues start off compact but can have null values if event targets are removed.
 	 * Periodically we compact them if too many null values exist.
 	 */
-	private _queues: {};
+	private _queues: Record<string,  Record<number, WeakRef<EventDispatcher>> | Record<number, EventDispatcher>> = {};
 	private static _instance: BroadcastEventDispatchQueue;
 	public static getInstance(): BroadcastEventDispatchQueue {
-		if (!BroadcastEventDispatchQueue._instance)
-			BroadcastEventDispatchQueue._instance = new BroadcastEventDispatchQueue();
-		return BroadcastEventDispatchQueue._instance;
-	}
-
-	constructor() {
-		this.reset();
-	}
-
-	reset() {
-		this._queues = {};
+		return BroadcastEventDispatchQueue._instance
+			|| (BroadcastEventDispatchQueue._instance = new BroadcastEventDispatchQueue());
 	}
 
 	add(type: string, target: EventDispatcher) {
 		// release || assert(Event.isBroadcastEventType(type), "Can only register broadcast events.");
 		const queue = this._queues[type] || (this._queues[type] = []);
-		if (queue.indexOf(target) >= 0) {
+
+		if (queue[target.id])
 			return;
-		}
-		queue.push(target);
+
+		queue[target.id] = (USE_WEAK ? new self.WeakRef(target) : target);
 	}
 
 	remove(type: string, target: EventDispatcher) {
 		//release || assert (Event.isBroadcastEventType(type), "Can only unregister broadcast events.");
 		const queue = this._queues[type];
 		release || assert(queue, 'There should already be a queue for this.');
-		const index = queue.indexOf(target);
-		release || assert(index >= 0, 'Target should be somewhere in this queue.');
-		queue[index] = null;
-		release || assert(queue.indexOf(target) < 0, 'Target shouldn\'t be in this queue anymore.');
+		delete queue[target.id];
 	}
 
 	dispatchEvent(event: Event) {
 		//release || assert (event.isBroadcastEvent(), "Cannot dispatch non-broadcast events.");
 		const queue = this._queues[event._type];
-		if (!queue) {
+
+		//return if no listeners exist for event type
+		if (!queue)
 			return;
-		}
-		/*
-		if (!release && traceEventsOption.value) {
-		  console.log('Broadcast event of type ' + event._type + ' to ' + queue.length +
-					  ' listeners');
-		}*/
-		let nullCount = 0;
-		for (let i = 0; i < queue.length; i++) {
-			const target = queue[i];
-			if (target === null) {
-				nullCount++;
+
+		for (const key in queue) {
+			let target = queue[key];
+			if (USE_WEAK)
+				target = (<WeakRef<EventDispatcher>> target)?.deref();
+			
+			if (target) {
+				(<EventDispatcher> target).dispatchEvent(event);
 			} else {
-				target.dispatchEvent(event);
+				console.debug('[BroadcastEventDispatchQueue] Broadcast target was deleted by GC:', key);
+				delete queue[key];
 			}
-		}
-		// Compact the queue if there are too many holes in it.
-		if (nullCount > 16 && nullCount > (queue.length >> 1)) {
-			const compactedQueue = [];
-			for (let i = 0; i < queue.length; i++) {
-				if (queue[i]) {
-					compactedQueue.push(queue[i]);
-				}
-			}
-			this._queues[event.type] = compactedQueue;
 		}
 	}
 
 	getQueueLength(type: string) {
-		return this._queues[type] ? this._queues[type].length : 0;
+		return this._queues[type] ? Object.getOwnPropertyNames(this._queues[type]).length : 0;
 	}
 }
