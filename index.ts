4a580b10cccd8c6f25322442b0a18da97d824791
Rob Bateman <rob@infiniteturtles.co.uk>
2023-08-29 23:12:21 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - PlayerGlobal - 0.2.378");
+console.debug("AwayFL - PlayerGlobal - 0.2.379");
 
 export { Sprite } from './lib/display/Sprite';
 export { MovieClip } from './lib/display/MovieClip';
