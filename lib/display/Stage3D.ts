22ea7bfcd7a2ac1de8505be35cbcda35cc730ec0
Paulo Medeiros <paulo.medeiros1302@gmail.com>
2024-09-12 22:06:06 +0000

    it almost works?

--- lib/display/Stage3D.ts
+++ lib/display/Stage3D.ts
@@ -2,57 +2,75 @@ import { ContextGLProfile, ContextMode, Stage as AwayStage, Stage, StageManager 
 import { EventDispatcher } from '../events/EventDispatcher';
 import { Context3D } from '../display3D/Context3D';
 import { Context3DProfile } from '../display3D/Context3DProfile';
-import { AVMStage } from '@awayfl/swf-loader';
-import { AXClass } from '@awayfl/avm2';
+import { AVMStage, Debug } from '@awayfl/swf-loader';
+import { Bytecode } from '@awayfl/avm2/lib/Bytecode';
+import { Multiname } from '@awayfl/avm2';
+import { Event } from '../events/Event';
+import { SecurityDomain } from '../SecurityDomain';
+
 
 export class Stage3D extends EventDispatcher {
-	static axClass: typeof Stage3D & AXClass;
-	static classInitializer: any = null;
+	// Called whenever the class is initialized.
+	public static classInitializer: any = null;
+
+	// List of static symbols to link.
+	public static classSymbols: string[] = null; // [];
+
+	// List of instance symbols to link.
+	public static instanceSymbols: string[] = null; // [];
 	private _context3D: Context3D
-	private _visible: Boolean
+	private _visible: boolean
 	private _x: number
 	private _y: number
-
-	private _adapteeStage3Ds: AwayStage[] = AVMStage.instance().stage3Ds
-	private _adaptee: AwayStage
+	private instance: number
 
 	constructor() {
+		// This is never called, what's going on???
 		super();
-
-		this._adapteeStage3Ds[this._adapteeStage3Ds.length] = StageManager.getInstance().getFreeStage();
-		console.log('Created Stage3D ' + (this._adapteeStage3Ds.length - 1));
-
+		console.log("Stage3D constructor called")
 	}
 
-	public get context3D(): Context3D {
-		return this._context3D;
+	public get x(): number {
+		Debug.notImplemented('[playerglobal/display/Stage3D] - get x not implemented');
+		return this._x;
 	}
 
-	public requestContext3D(context3DRenderMode: String = 'auto', profile: String = 'baseline'): void {
-		this._context3D = new Context3D(this, this._adaptee, profile);
-		const forceSoftware: boolean = (context3DRenderMode == 'auto');
-		switch (profile) {
-			case Context3DProfile.BASELINE:
-				this._adaptee.requestContext(forceSoftware, ContextGLProfile.BASELINE);
-				break;
-			case Context3DProfile.BASELINE_CONSTRAINED:
-				this._adaptee.requestContext(forceSoftware, ContextGLProfile.BASELINE_CONSTRAINED);
-				break;
-			case Context3DProfile.BASELINE_EXTENDED:
-				this._adaptee.requestContext(forceSoftware, ContextGLProfile.BASELINE_EXTENDED);
-				break;
-			case Context3DProfile.STANDARD:
-				console.log('Unsupported Context3D Profile \'standard\' Requested');
-				break;
-			case Context3DProfile.STANDARD_CONSTRAINED:
-				console.log('Unsupported Context3D Profile \'standard_constrained\' Requested');
-				break;
-			case Context3DProfile.STANDARD_EXTENDED:
-				console.log('Unsupported Context3D Profile \'standard_extended\' Requested');
-				break;
-			default:
-				break;
+	public set x(value: number) {
+		this._x = value;
+		Debug.notImplemented('[playerglobal/display/Stage3D] - set x not implemented');
+	}
 
-		}
+	public get y(): number {
+		Debug.notImplemented('[playerglobal/display/Stage3D] - get y not implemented');
+		return this._y;
+	}
+
+	public set y(value: number) {
+		value = this._y;
+		Debug.notImplemented('[playerglobal/display/Stage3D] - set y not implemented');
+	}
+
+	public get visible(): boolean {
+		Debug.notImplemented('[playerglobal/display/Stage3D] - get visible not implemented');
+		return this._visible;
+	}
+
+	public set visible(value: boolean) {
+		this._visible = value
+		Debug.notImplemented('[playerglobal/display/Stage3D] - set visible not implemented');
+	}
+
+	public get context3D():Context3D {
+		return this._context3D
+	}
+
+	public requestContext3D(context3DRenderMode: string = 'auto', profile: string = 'baseline'): void {
+		console.log("Request Context")
+		this._context3D = new Context3D(0, context3DRenderMode, profile ?? 'baseline' , this);
+		this._context3D.addEventListener(Event.CONTEXT3D_CREATE, this.onContextCreated)
+	}
+
+	public onContextCreated(e:Event=null){
+		super.dispatchEvent(new Event(Event.CONTEXT3D_CREATE))
 	}
 }
