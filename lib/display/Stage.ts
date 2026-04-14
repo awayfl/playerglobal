8935fc79ca3f40f0d2d00ac68da59caac2beb578
Rob Bateman <rob@infiniteturtles.co.uk>
2026-04-14 20:23:20 +0100

    implement Stage.quality

--- lib/display/Stage.ts
+++ lib/display/Stage.ts
@@ -6,7 +6,7 @@ import { DisplayObjectContainer } from './DisplayObjectContainer';
 import { DisplayObject } from './DisplayObject';
 import { FrameScriptManager } from '@awayjs/scene';
 import { View } from '@awayjs/view';
-import { Stage as AwayStage } from '@awayjs/stage';
+import { Stage as AwayStage, StageQuality } from '@awayjs/stage';
 import { DisplayObjectContainer as AwayDisplayObjectContainer, MouseEvent as MouseEventAway } from '@awayjs/scene';
 import { Transform } from '../geom/Transform';
 import { Rectangle } from '../geom/Rectangle';
@@ -722,15 +722,12 @@ export class Stage extends DisplayObjectContainer {
 	 *   For more information, see the "Security" chapter in the ActionScript 3.0 Developer's Guide.
 	 */
 	public get quality (): string {
-		// @todo
-		Debug.throwPIR('playerglobals/display/Stage', 'get quality', '');
-		return '';
+		return AVMStage.instance().quality.toUpperCase();
 
 	}
 
 	public set quality (value: string) {
-		// @todo
-		Debug.throwPIR('playerglobals/display/Stage', 'set quality', '');
+		AVMStage.instance().quality = <StageQuality> value.toLowerCase();
 	}
 
 	/**
