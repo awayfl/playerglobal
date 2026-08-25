2378c4784b8cf9fa3b1f96b773ff08c41696b857
Francis <francisperez19@gmail.com>
2026-08-25 19:56:50 +0800

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - PlayerGlobal - 0.2.445");
+console.debug("AwayFL - PlayerGlobal - 0.2.446");
 
 export { Sprite } from './lib/display/Sprite';
 export { MovieClip } from './lib/display/MovieClip';
